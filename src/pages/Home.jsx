f2f331a956f46b35c02aadad31959e4b31e85f57
aswin0603 <aswinbalakrishnan0603@gmail.com>
2025-11-18 10:23:40 +0530

    Add existing project files to Git

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+return (
+  <div className="page-container">
+    <h1>Home</h1>
+    <p>Welcome</p>
+    <a href="/login">
+      <button>Login</button>
+    </a>
+  </div>
+);
+}
